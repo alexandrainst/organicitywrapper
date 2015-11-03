f0544ff49f689a87a9a7640edfbb10dc1a6316b1
lassevestergaard <lasse.vestergaard@alexandra.dk>
2015-11-03 14:32:24 +0100

    Made template for rest endpoint

--- oc-history-service-aak/src/main/java/dk/alexandra/organicity/MyResource.java
+++ oc-history-service-aak/src/main/java/dk/alexandra/organicity/MyResource.java
@@ -2,13 +2,14 @@ package dk.alexandra.organicity;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("organicity")
 public class MyResource {
 
     /**
@@ -19,7 +20,23 @@ public class MyResource {
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    public String defaultView() {
+        return "This api has two calls: /entities/{id}/readings and /entities. \n\n"+
+        		"/entities has the following parameters: latStart (default null), latEnd (default null), longStart (default null), longEnd (default null), limit (default 100) ";
+    }
+    
+    
+    @GET
+    @Path("/entities")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getAllCustomers() {
+      return "---Customer List---";
+    }
+    
+    @GET
+    @Path("/entities/{id}/readings")
+    @Produces(MediaType.TEXT_PLAIN)
+    public long getCustomer(@PathParam("id") long id) {
+      return id;
     }
 }
